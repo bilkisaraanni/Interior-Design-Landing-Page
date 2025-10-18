5f57b4b41dece15b269376000ee5243054a5a8f9
bilkisaraanni <bilkisara28@mail.com>
2025-10-19 01:23:04 +0600

    anni

---
+++ script.js
@@ -0,0 +1,50 @@
+// nav part 
+let menu = document.querySelector('.fa-bars');
+let navbtn = document.querySelector('.buttres');
+let navulres = document.querySelector('.navulres');
+let show = false;
+menu.addEventListener('click', () => {
+     if(!show){
+        navulres.style.display = "block"; 
+        navbtn.style.display = "block"; 
+        
+        show = true;
+     }else{
+        navulres.style.display = "none";
+        show = false;
+     }
+})
+
+// bannar part countdown 
+
+let count = document.querySelector('.count');
+let countone = document.querySelector('.countone'); // Fixed selector
+let counttwo = document.querySelector('.counttwo');
+
+let counter = 0;
+let counterOne = 0;
+let counterTwo = 0;
+
+let intervel = setInterval(() => {
+    count.textContent = counter;
+    counter++
+    if( counter > 400){
+       clearInterval(intervel)
+    }
+},100);
+
+let intervelOne = setInterval(()=>{
+    countone.textContent = counterOne;
+    counterOne++
+    if(counterOne > 600){
+      clearInterval(intervelOne)
+    }
+},100);
+
+let intervelTwo = setInterval(()=>{
+     counttwo.textContent = counterTwo;
+     counterTwo++
+     if(counterTwo > 100){
+      clearInterval(intervelTwo)
+     }
+},100);
